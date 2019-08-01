a0e5d04a7d8fabfe9b78a38561f88632418d9027
Amber <13611718+mxamber@users.noreply.github.com>
2019-08-01 18:46:01 +0200

    file input, user links, design improvements
    replaced text field with a file input element, added clickable links to
    sender and recipient of a convo, adapted design to different screen
    sizes by altering css to use vh instead of %

--- script.js
+++ script.js
@@ -1,18 +1,52 @@
-function convo() {
-	var raw = "";
-	document.getElementById("inner").innerHTML = "";
+function $(expr) {
+	return document.querySelector(expr);
+}
+
+function parse() {
+	var file = $("#upload").files[0];
 	
+	if(!file) {
+		console.log("no file!");
+		return;
+	} else {
+		console.log("file located!");
+	}
 	
-	raw = document.getElementById("input").value;
+	var reader = new FileReader();
+	reader.onload = function() {
+		console.log("reader reading!");
+		$("#inner").innerHTML = reader.result;
+		convo(reader.result);
+	}
+	
+	reader.readAsText(file);
+}
+
+function convo(raw) {
+	var wait = $("#wait");
+	var top = $("#top");
+	var a_send = $("#sender");
+	var a_recp = $("#recipient");
+	
+	wait.style.display = "block";
+	
 	var convo = JSON.parse(raw).dmConversation;
+	
+	$("#inner").innerHTML = "";
 	var members = convo.conversationId.split("-");
+	var sender = members[members.length - 1];
+	
+	a_send.setAttribute("href", "https://twitter.com/intent/user?user_id=" + sender);
+	a_recp.setAttribute("href", "https://twitter.com/intent/user?user_id=" + members[0]);
 	
 	convo.messages.reverse();
+	
+	var counter = 0;
 	
 	convo.messages.forEach(function(item) {
 		var msgC = item.messageCreate;
 		var msg = document.createElement("div");
-		if(msgC.senderId == members[1]) {
+		if(msgC.senderId == sender) {
 			msg.setAttribute("class", "message1");
 		} else {
 			msg.setAttribute("class", "message2");
@@ -23,6 +57,10 @@ function convo() {
 		var timep = document.createElement("p");
 		timep.innerHTML = datestring;
 		msg.appendChild(timep);
-		document.getElementById("inner").appendChild(msg);
+		$("#inner").appendChild(msg);
+		counter++;
+		if(counter >= convo.messages.length) {
+			wait.style.display = "none";
+		}
 	});
 }
